09a4de2b1cb6896c430146e777ba0cca94708e6e
nickzamboli <nzambolidesign@gmail.com>
2017-03-09 09:27:02 -0500

    Angular 2 Tutorial -- Part 5 of Tutorial

---
+++ src/app/dashboard.component.ts
@@ -0,0 +1,12 @@
+import { Component,OnInit } from '@angular/core';
+
+import {}
+
+@Component({
+  moduleId: module.id,
+  selector:'my-dashboard',
+  templateUrl: './dashboard.component.html'
+})
+export class DashboardComponent {
+
+}
